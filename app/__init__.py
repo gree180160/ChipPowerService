f10e01e39025aabd9ca656d343db8abac2ce9727
15976988260 <1459287460@qq.com>
2026-08-31 15:29:34 +0800

    1.修改爬虫读数据接口

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 import threading
 import time
-from flask import Flask
+import gzip
+from flask import Flask, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 from app.config import Config
 
 db = SQLAlchemy()
+
+# 超过该字节数的 JSON 响应才启用 gzip 压缩，避免大响应在远程网络上被截断
+GZIP_MIN_LENGTH = 2048
 
 
 def create_app(config_class=Config):
@@ -15,6 +19,25 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     CORS(app)
+
+    # 对较大的 JSON 响应启用 gzip，避免大响应(尤其单行 JSON)在远程网络上传输时被中间层截断
+    @app.after_request
+    def compress_large_responses(response):
+        if 'gzip' not in request.headers.get('Accept-Encoding', ''):
+            return response
+        if response.mimetype != 'application/json' or not response.is_sequence:
+            return response
+        data = response.get_data()
+        if data is None or len(data) < GZIP_MIN_LENGTH:
+            return response
+        compressed = gzip.compress(data, compresslevel=6)
+        if len(compressed) >= len(data):
+            return response
+        response.set_data(compressed)
+        response.headers['Content-Encoding'] = 'gzip'
+        response.headers['Vary'] = 'Accept-Encoding'
+        response.headers['Content-Length'] = str(len(compressed))
+        return response
 
     from app.controllers.data_controller import data_bp
     app.register_blueprint(data_bp, url_prefix='/api/data')
